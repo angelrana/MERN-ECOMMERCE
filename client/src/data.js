6393355fb56b15553ec4906be40669e394effae6
Angel Rana <88431133+angelrana@users.noreply.github.com>
2022-08-08 20:54:14 +0530

    commit

---
+++ client/src/data.js
@@ -0,0 +1,47 @@
+export const SliderImages = [
+  {
+    id: 1,
+    title: "SUMMER OUTFIT",
+    desc: "DON'T MISS OUT ON THESE AMAZING SUMMER CASUAL OUTFIT. NOW ON SALE!",
+    img: "https://cdn.vox-cdn.com/thumbor/3lBMs8jxFOgY5HUbeQ7dBvNLncs=/0x0:2102x1488/1200x800/filters:focal(978x425:1314x761)/cdn.vox-cdn.com/uploads/chorus_image/image/53213839/carbon_38_ready_to_wear.0.jpg",
+    bg: "fcf1ed",
+    cat: "shirt",
+  },
+  {
+    id: 2,
+    title: "AUTUMN OUTFIT",
+    desc: "DON'T MISS OUT ON THESE AMAZING SUMMER CASUAL OUTFIT. NOW ON SALE!",
+    img: "https://img.freepik.com/free-photo/graceful-european-woman-winter-fur-coat-stylish-dress-sitting-wearing-ankle-boot-black-leather_273443-4012.jpg?size=626&ext=jpg&ga=GA1.2.1257176031.1656180328",
+    bg: "f5fafd",
+    cat: "loungewear",
+  },
+  {
+    id: 1,
+    title: "WINTER Outfit",
+    desc: "DON'T MISS OUT ON THESE AMAZING SUMMER CASUAL OUTFIT. NOW ON SALE!",
+    img: "https://img.freepik.com/free-photo/fashionable-model-stylish-hat-red-coat-boots-posing-white-wall-studio_273443-4646.jpg?size=626&ext=jpg&ga=GA1.2.1257176031.1656180328",
+    bg: "f5fafd",
+    cat: "jackets",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: "https://i.pinimg.com/736x/81/a8/a9/81a8a9770a1279e02fd64eeea3b212c8.jpg",
+    title: "SHIRT STYLE",
+    cat: "shirt",
+  },
+  {
+    id: 2,
+    img: "https://images.squarespace-cdn.com/content/v1/5a5596188dd0411a8df1b41a/1611966720703-UAQKYPY0DZC05EH51XNN/Blush+Mark.jpg",
+    title: "LOUNGEWEAR LOVE",
+    cat: "loungewear",
+  },
+  {
+    id: 3,
+    img: "https://lh3.googleusercontent.com/cBQkhuN1oXsaxS1Uv-gKGdefeDIyPG5G4WYAcxEqQlP5entolOCytVcvToKQZxRWolhcn1MIICWC8S0nN0a23Z9kTbg=w1000",
+    title: "LIGHT JACKETS",
+    cat: "jackets",
+  },
+];
