8556a0884fa221eec18c3a28cd602c0b3701b2af
Angel Rana <88431133+angelrana@users.noreply.github.com>
2022-08-08 20:52:13 +0530

    Add files via upload

---
+++ client/src/redux/wishlistRedux.js
@@ -0,0 +1,17 @@
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const wishlistSlice = createSlice({
+//   name: "wishlist",
+//   initialState: {
+//     product: [],
+//   },
+//   reducers: {
+//     wishlistProduct: (state, action) => {
+//       state.product += action.payload;
+//     },
+//   },
+// });
+
+// export const { wishlistProduct } = wishlistSlice.actions;
+
+// export default wishlistSlice.reducer;
